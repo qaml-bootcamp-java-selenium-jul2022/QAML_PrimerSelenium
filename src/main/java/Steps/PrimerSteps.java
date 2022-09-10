63ebd72c74104304413a2baadb08d0238490d89e
Alvaro Meraz <alvmmn@gmail.com>
2022-09-10 10:41:44 -0500

    Tarea Selenium #1 agregar metodos a PrimerSteps
    Tarea Selenium #1 agregar metodos a PrimerSteps

--- src/main/java/Steps/PrimerSteps.java
+++ src/main/java/Steps/PrimerSteps.java
@@ -6,4 +6,19 @@ public class PrimerSteps extends BaseSteps{
     public PrimerSteps(WebDriver webDriver) {
         super(webDriver);
     }
+    public void abrirURLEnBrowser(){
+        myWebDriver.get("https:www.google.com");
+    }
+
+    public void obtenerURLPagina(){
+        System.out.println("La url de la pagina es "+myWebDriver.getCurrentUrl());
+    }
+
+    public void obtenerTituloPaginaAbierta(){
+        System.out.println("El título de la página es "+ myWebDriver.getTitle());
+    }
+    public void cerrarBrowser(){
+        myWebDriver.quit();
+    }
+
 }
