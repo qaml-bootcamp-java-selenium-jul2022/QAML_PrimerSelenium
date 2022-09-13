98c6b0a452a5089d7c8899eb82f8881a2a66019e
Denisse Monsserrath Vargas Yañez <rouge0051@gmail.com>
2022-09-13 09:26:20 -0500

    Segunda Tarea Selenium

--- src/main/java/Pages/MyFirstWebPage.java
+++ src/main/java/Pages/MyFirstWebPage.java
@@ -1,4 +1,25 @@
 package Pages;
 
-public class MyFirstWebPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class MyFirstWebPage extends BasePage{
+
+    public MyFirstWebPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public WebElement getInputName(){
+        return myWebDriver.findElement(By.name("username"));
+    }
+
+    public WebElement getInputPassword(){
+        return myWebDriver.findElement(By.name("password"));
+    }
+
+    public WebElement getButton(){
+        return myWebDriver.findElement(By.tagName("button"));
+    }
+
 }
