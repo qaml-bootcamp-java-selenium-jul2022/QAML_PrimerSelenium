38e6096cf289ac1c0a5d04122dd8f739504b350f
Alvaro Meraz <alvmmn@gmail.com>
2022-09-14 19:34:43 -0500

    agrego clases nuevas

---
+++ src/main/java/Pages/DemoQAPage.java
@@ -0,0 +1,10 @@
+package Pages;
+
+import Steps.BaseSteps;
+import org.openqa.selenium.WebDriver;
+
+public class DemoQAPage extends BasePage {
+    public DemoQAPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+}
---
+++ src/main/java/Steps/YouTubeSteps.java
@@ -0,0 +1,18 @@
+package Steps;
+
+import Pages.YouTubePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class YouTubeSteps extends BaseSteps{
+    public YouTubeSteps(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    YouTubePage paginaYouTube = new YouTubePage(myWebDriver);
+
+    public void escribirTextoSearchBox(String texto) {
+        WebElement searchBox = paginaYouTube.getYouTubeSearchBox();
+        searchBox.sendKeys(texto);
+    }
+}
